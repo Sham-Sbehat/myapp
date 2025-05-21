b400450e94eab4773f7110fbb0523058cd41f110
Sham Sbehat <121432620+Sham-Sbehat@users.noreply.github.com>
2025-05-21 16:00:13 +0300

    edit

--- src/Pages/PageNotFound/PageNotFound.jsx
+++ src/Pages/PageNotFound/PageNotFound.jsx
@@ -1,10 +1,29 @@
+// src/pages/NotFound.jsx
+import React from 'react';
+import { Box, Typography, Button, Container, Paper } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
 
-import imageNotFound from '../../assets/images/PageNotFound.jpg'
-import styles from './PageNotFound.module.scss'; 
-export default function PageNotFound() {
+const NotFound = () => {
+  const navigate = useNavigate();
+
   return (
-    <div  >
-      <img src={imageNotFound} className={styles.img} alt="Page Not Found" />
-    </div>
+    <Container maxWidth="md">
+      <Paper elevation={3} sx={{ mt: 10, p: 6, textAlign: 'center', width:'100%',marginLeft:'300px' }}>
+        <Typography variant="h1" color="error" gutterBottom>
+          404
+        </Typography>
+        <Typography variant="h5" gutterBottom>
+          Pagge Not Found
+        </Typography>
+        <Typography variant="body1" color="textSecondary" sx={{ mb: 4 }}>
+          Sorry , Page Not Exist
+        </Typography>
+        <Button variant="contained" color="primary" onClick={() => navigate('/')}>
+         Return Back 
+        </Button>
+      </Paper>
+    </Container>
   );
-}
+};
+
+export default NotFound;
