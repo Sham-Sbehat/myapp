c049863be93c801a2ef1c8650b908299dca8205d
Sham Sbehat <121432620+Sham-Sbehat@users.noreply.github.com>
2025-05-23 15:28:12 +0300

    edit 404

--- src/Pages/PageNotFound/PageNotFound.jsx
+++ src/Pages/PageNotFound/PageNotFound.jsx
@@ -1,28 +1,84 @@
 // src/pages/NotFound.jsx
-import React from 'react';
-import { Box, Typography, Button, Container, Paper } from '@mui/material';
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { Box, Typography, Button, Container, Paper } from "@mui/material";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
   const navigate = useNavigate();
 
   return (
-    <Container maxWidth="md">
-      <Paper elevation={3} sx={{ mt: 10, p: 6, textAlign: 'center', width:'100%',marginLeft:'300px' }}>
-        <Typography variant="h1" color="error" gutterBottom>
+    <Box
+      sx={{
+        height: "100vh",
+        width: "100vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#f9f9f9",
+        flexDirection: "column",
+      }}
+    >
+      <Paper
+        elevation={6}
+        sx={{
+          p: 6,
+          borderRadius: 4,
+          backgroundColor: "#fff",
+          textAlign: "center",
+          width: "100%",
+          maxWidth: "600px",
+        }}
+      >
+        <Typography
+          variant="h4"
+          sx={{
+            color: "#a11925",
+            fontWeight: "bold",
+            mb: 4,
+          }}
+        >
+          Pagge Not Found!
+        </Typography>
+        <Typography
+          variant="h3"
+          gutterBottom
+          sx={{
+            color: "#a11925",
+            fontWeight: "bold",
+            textAlign: "center",
+          }}
+        >
           404
         </Typography>
-        <Typography variant="h5" gutterBottom>
-          Pagge Not Found
-        </Typography>
-        <Typography variant="body1" color="textSecondary" sx={{ mb: 4 }}>
-          Sorry , Page Not Exist
-        </Typography>
-        <Button variant="contained" color="primary" onClick={() => navigate('/')}>
-         Return Back 
-        </Button>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            gap: 4,
+            mt: 5,
+            flexWrap: "wrap",
+          }}
+        >
+          <Button
+            variant="contained"
+            onClick={() => navigate("/")}
+            sx={{
+              backgroundColor: "#a11925",
+              "&:hover": {
+                backgroundColor: "#821a1a",
+              },
+              px: 5,
+              py: 2,
+              fontSize: "1.1rem",
+              borderRadius: 3,
+              minWidth: "180px",
+            }}
+          >
+            Back to Home
+          </Button>
+        </Box>
       </Paper>
-    </Container>
+    </Box>
   );
 };
 
