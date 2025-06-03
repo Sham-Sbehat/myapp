85bbcf22870822848c1634b96f4b58696fa0a5f0
Sham Sbehat <121432620+Sham-Sbehat@users.noreply.github.com>
2025-06-03 23:05:04 +0300

    edit

--- src/Components/HookComponents/TemplateField/TemplateField.jsx
+++ src/Components/HookComponents/TemplateField/TemplateField.jsx
@@ -1,23 +1,34 @@
 import { MenuItem } from "@mui/material";
 import { StyledBox, StyledTextField } from "./TemplateForm.styled";
+import { Controller } from "react-hook-form";
 
-const TemplateForm = ({ register, errors }) => {
+const TemplateField = ({ control, errors, index }) => {
   return (
     <StyledBox>
-      <StyledTextField
-        select
-        fullWidth
-        label="Workspace Template"
-        {...register("template", { required: "Please select a template" })}
-        error={!!errors.template}
-        helperText={errors.template?.message}
-      >
-        <MenuItem value="">Select</MenuItem>
-        <MenuItem value="templateA">Template A</MenuItem>
-        <MenuItem value="templateB">Template B</MenuItem>
-      </StyledTextField>
+      <Controller
+        name={`template[${index}]`}
+        control={control}
+        defaultValue=""
+        rules={{ required: "Please select a template" }}
+        render={({ field }) => (
+          <StyledTextField
+            select
+            fullWidth
+            label={`Workspace Template #${index + 1}`}
+            value={field.value}
+            onChange={field.onChange}
+            error={!!errors?.template?.[index]}
+            helperText={errors?.template?.[index]?.message}
+          >
+            <MenuItem value="">Select</MenuItem>
+            <MenuItem value="templateA">Template A</MenuItem>
+            <MenuItem value="templateB">Template B</MenuItem>
+            <MenuItem value="templateC">Template C</MenuItem>
+          </StyledTextField>
+        )}
+      />
     </StyledBox>
   );
 };
 
-export default TemplateForm;
+export default TemplateField;
--- src/Pages/AddPatient/AddPatientHookForm.jsx
+++ src/Pages/AddPatient/AddPatientHookForm.jsx
@@ -5,9 +5,11 @@ import {
   Typography,
   Box,
   Grid,
+  Button,
 } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useForm } from "react-hook-form";
+import { useState } from "react";
 import FirstLastNameFields from "../../Components/HookComponents/FirstLastName/FirstLastNameFields";
 import GenderField from "../../Components/HookComponents/Gender/GenderField";
 import DateOfBirthField from "../../Components/HookComponents/DateOfBirth/DateOfBirthField";
@@ -30,6 +32,7 @@ const theme = createTheme({
 
 const AddPatientFormHook = () => {
   const navigate = useNavigate();
+  const [templateCount, setTemplateCount] = useState(1); // عدد حقول TemplateField
 
   const {
     register,
@@ -43,41 +46,57 @@ const AddPatientFormHook = () => {
   } = useForm({
     defaultValues: {
       disorders: {},
+      template: [''],  // هنا تأكدت انه template له قيمة ابتدائية (فارغة) لتجنب undefined
     },
   });
 
-  const onSubmit = (data) => {
-    const watchedDisorders = watch("disorders") || {};
+const onSubmit = (data) => {
+  // disorders:
+  const watchedDisorders = watch("disorders") || {};
+  const selectedDisorders = Object.entries(watchedDisorders)
+    .filter(([, value]) => value)
+    .map(([key]) => key);
 
-    const selectedDisorders = Object.entries(watchedDisorders)
-      .filter(([, value]) => value)
-      .map(([key]) => key);
+  if (selectedDisorders.length === 0) {
+    setError("disorders", {
+      type: "manual",
+      message: "Please select at least one disorder",
+    });
+    return;
+  } else {
+    clearErrors("disorders");
+  }
 
-    if (selectedDisorders.length === 0) {
-      setError("disorders", {
-        type: "manual",
-        message: "Please select at least one disorder",
-      });
-      return;
-    } else {
-      clearErrors("disorders");
-    }
+  // newPatient:
+  const newPatient = { ...data, disorders: selectedDisorders };
 
-    const newPatient = { ...data, disorders: selectedDisorders };
+  // patients from localStorage:
+  const savedPatients = localStorage.getItem("patients");
+  const patients = savedPatients ? JSON.parse(savedPatients) : [];
+  const updatedPatients = [...patients, newPatient];
+  localStorage.setItem("patients", JSON.stringify(updatedPatients));
 
-  
-    const savedPatients = localStorage.getItem("patients");
-    const patients = savedPatients ? JSON.parse(savedPatients) : [];
+  // data.template is an object or array? بسبب تسميتنا template[index], هي مصفوفة
+  const templates = data.template || [];
 
-    const updatedPatients = [...patients, newPatient];
+  alert(
+    `Patient Info:\n\n` +
+      `First Name: ${data.firstName}\n` +
+      `Last Name: ${data.lastName}\n` +
+      `Gender: ${data.gender}\n` +
+      `Date of Birth: ${data.dateOfBirth}\n` +
+      `Disorders: ${selectedDisorders.join(", ") || "None"}\n` +
+      `Templates: ${templates.filter(Boolean).join(", ") || "None"}`
+  );
+
+  reset();
+  navigate("/patients");
+};
 
 
-    localStorage.setItem("patients", JSON.stringify(updatedPatients));
-
-  
-    reset();
-    navigate("/patients");
-  };
+  // const handleAddTemplate = () => {
+  //   setTemplateCount((prev) => prev + 1);
+  // };
 
   return (
     <ThemeProvider theme={theme}>
@@ -92,7 +111,23 @@ const AddPatientFormHook = () => {
               <GenderField register={register} control={control} errors={errors} />
               <DateOfBirthField register={register} errors={errors} />
               <DisordersField register={register} watch={watch} errors={errors} />
-              <TemplateField register={register} errors={errors} />
+
+              {[...Array(templateCount)].map((_, index) => (
+  <TemplateField
+    key={index}
+    control={control}
+    errors={errors}
+    index={index}
+  />
+))}
+
+<Grid item xs={12}>
+  <Button variant="outlined" color="primary" onClick={() => setTemplateCount(c => c + 1)}>
+    Add Workspace
+  </Button>
+</Grid>
+
+
               <FormButtons />
             </Grid>
           </Box>
